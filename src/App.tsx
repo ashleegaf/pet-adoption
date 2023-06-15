fce65314af02983bfc692a018eddd14dca4462e5
ashleegaf <ashleegaf@gmail.com>
2023-06-15 17:28:24 -0400

    Apply prettier settings to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import Pet from './components/Pet';
+import SearchParams from './components/SearchParams';
 
 const App = () => {
-  return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <Pet name='Luna' animal='dog' breed='bulldog' />
-      <Pet name='Pepper' animal='bird' breed='cockatiel' />
-      <Pet name='Greeny' animal='cat' breed='Mixed' />
-    </div>
-  );
+	return (
+		<div>
+			<h1>Adopt Me!</h1>
+			<SearchParams />
+		</div>
+	);
 };
 
 export default App;
--- src/components/Pet.tsx
+++ src/components/Pet.tsx
@@ -1,15 +1,15 @@
 interface PetProps {
-  [k: string]: string
+	[k: string]: string;
 }
 
 const Pet = ({ name, animal, breed }: PetProps) => {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
-  );
+	return (
+		<div>
+			<h1>{name}</h1>
+			<h2>{animal}</h2>
+			<h2>{breed}</h2>
+		</div>
+	);
 };
 
 export default Pet;
