07a9b05cd5c0ed1afcfcdbef16e73b3d548a27e2
ashleegaf <ashleegaf@gmail.com>
2023-06-16 16:23:00 -0400

    Initialize query client in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import SearchParams from './components/SearchParams';
 import Details from './components/Details';
+
+// initialize react query's cache client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity
+    }
+  }
+})
 
 const App = () => {
 	return (
 		<BrowserRouter>
-      <header>
-        <Link to='/'>Adopt Me!</Link>
-      </header>
-			<Routes>
-				<Route path='/details/:id' element={<Details />} />
-				<Route path='/' element={<SearchParams />} />
-			</Routes>
+      {/* expose the cache context to to all components */}
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to='/'>Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path='/details/:id' element={<Details />} />
+          <Route path='/' element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
 		</BrowserRouter>
 	);
 };
