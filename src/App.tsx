6413866353e98fb378469ad9402520c597fcdca3
ashleegaf <ashleegaf@gmail.com>
2023-06-16 20:11:45 -0400

    Rename pet details component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
 import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import SearchParams from './components/SearchParams';
-import Details from './components/Details';
+import SearchParams from './Search/SearchParams';
+import PetDetails from './Details/PetDetails';
 
 // initialize react query's cache client
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Infinity,
-      cacheTime: Infinity
-    }
-  }
-})
+	defaultOptions: {
+		queries: {
+			staleTime: Infinity,
+			cacheTime: Infinity,
+		},
+	},
+});
 
 const App = () => {
 	return (
 		<BrowserRouter>
-      {/* expose the cache context to to all components */}
-      <QueryClientProvider client={queryClient}>
-        <header>
-          <Link to='/'>Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path='/details/:id' element={<Details />} />
-          <Route path='/' element={<SearchParams />} />
-        </Routes>
-      </QueryClientProvider>
+			{/* expose the cache context to to all components */}
+			<QueryClientProvider client={queryClient}>
+				<header>
+					<Link to='/'>Adopt Me!</Link>
+				</header>
+				<Routes>
+					<Route path='/details/:id' element={<PetDetails />} />
+					<Route path='/' element={<SearchParams />} />
+				</Routes>
+			</QueryClientProvider>
 		</BrowserRouter>
 	);
 };
