7a6815f0f35ddb285a2ba3c17420c6a590b8b0a3
ashleegaf <ashleegaf@gmail.com>
2023-06-16 20:14:11 -0400

    Move pet component and utils into separate folder

---
+++ src/Details/PetDetails.tsx
@@ -0,0 +1,36 @@
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import fetchPet from './fetchPet';
+
+const PetDetails = () => {
+	const { id } = useParams();
+	// if the query key isn't in cache, call fetchPet()
+	const results = useQuery(['details', id || ''], fetchPet);
+
+	if (results.isError) {
+		return <h2>Error loading pet details</h2>;
+	} else if (results.isLoading) {
+		return (
+			<div className='loading-pane'>
+				<h2 className='loader'>🌀</h2>
+			</div>
+		);
+	}
+
+	const { name, animal, city, state, description, breed, images } =
+		results.data.pets[0];
+
+	return (
+		<div className='details'>
+			<h2>{name}</h2>
+			<img src={images[0]} alt={name}></img>
+			<h2>
+				{animal} - {breed} - {city}, {state}
+			</h2>
+			<button>Adopt {name}</button>
+			<p>{description}</p>
+		</div>
+	);
+};
+
+export default PetDetails;
