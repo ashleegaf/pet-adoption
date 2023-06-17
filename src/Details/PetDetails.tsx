298095a0b30220421a0a8994328d2c7fd61291e1
ashleegaf <ashleegaf@gmail.com>
2023-06-17 17:36:07 -0400

    Update pet details modal to return home when pet is adopted

--- src/Details/PetDetails.tsx
+++ src/Details/PetDetails.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useContext } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
+import AdoptedPetContext from '../AdoptedPetContext';
 import ErrorBoundary from './ErrorBoundary';
 import fetchPet from './fetchPet';
 import Carousel from './Carousel';
@@ -8,7 +9,15 @@ import Modal from './Modal';
 
 const PetDetails = () => {
 	const [showModal, setShowModal] = useState(false);
+
+	const navigate = useNavigate();
+
+	// access adoptedPet hook from App
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [_, setAdoptedPet] = useContext(AdoptedPetContext);
+
 	const { id } = useParams();
+
 	// if the query key isn't in cache, call fetchPet()
 	const results = useQuery(['details', id || ''], fetchPet);
 
@@ -22,8 +31,8 @@ const PetDetails = () => {
 		);
 	}
 
-	const { name, animal, city, state, description, breed, images } =
-		results.data.pets[0];
+	const pet = results.data.pets[0];
+	const { name, animal, city, state, description, breed, images } = pet;
 
 	return (
 		<div className='details'>
@@ -34,12 +43,21 @@ const PetDetails = () => {
 			</h2>
 			<button onClick={() => setShowModal(true)}>Adopt {name}</button>
 			<p>{description}</p>
+			{/* confirm when users click to adopt a pet. if yes, set adopted 
+			pet in context and return home. if no, close modal */}
 			{showModal ? (
 				<Modal>
 					<div>
 						<h1>Would you like to adopt {name}?</h1>
 						<div className='buttons'>
-							<button>Yes</button>
+							<button
+								onClick={() => {
+									setAdoptedPet(pet);
+									navigate('/');
+								}}
+							>
+								Yes
+							</button>
 							<button onClick={() => setShowModal(false)}>No</button>
 						</div>
 					</div>
