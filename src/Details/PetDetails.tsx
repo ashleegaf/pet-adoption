613427b74f5faeef7a4e4775ab2ba9b646e0347b
ashleegaf <ashleegaf@gmail.com>
2023-06-17 13:00:48 -0400

    Create logic for displaying modal when "adopt pet" is clicked

--- src/Details/PetDetails.tsx
+++ src/Details/PetDetails.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import ErrorBoundary from './ErrorBoundary';
 import fetchPet from './fetchPet';
 import Carousel from './Carousel';
+import Modal from './Modal';
 
 const PetDetails = () => {
+	const [showModal, setShowModal] = useState(false);
 	const { id } = useParams();
 	// if the query key isn't in cache, call fetchPet()
 	const results = useQuery(['details', id || ''], fetchPet);
@@ -29,8 +32,19 @@ const PetDetails = () => {
 			<h2>
 				{animal} - {breed} - {city}, {state}
 			</h2>
-			<button>Adopt {name}</button>
+			<button onClick={() => setShowModal(true)}>Adopt {name}</button>
 			<p>{description}</p>
+			{showModal ? (
+				<Modal>
+					<div>
+						<h1>Would you like to adopt {name}?</h1>
+						<div className='buttons'>
+							<button>Yes</button>
+							<button onClick={() => setShowModal(false)}>No</button>
+						</div>
+					</div>
+				</Modal>
+			) : null}
 		</div>
 	);
 };
